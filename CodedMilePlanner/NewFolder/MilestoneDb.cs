b45c0dfbeac276936edad877ef9f3f4ecec7333f
rtaggart16 <rosstaggart44@gmail.com>
2019-04-02 11:58:44 +0100

    Added descriptive XML comments to the MilestoneDb class

--- CodedMilePlanner/NewFolder/MilestoneDb.cs
+++ CodedMilePlanner/NewFolder/MilestoneDb.cs
@@ -11,12 +11,31 @@ using System.Threading.Tasks;
 
 namespace CodedMilePlanner.NewFolder
 {
+    /// <summary>
+    /// Database class that defines the tables required for the application and provide
+    /// methods for database interaction 
+    /// </summary>
     public class MilestoneDb : IdentityDbContext<User>
     {
+        /// <summary>
+        /// Table that contains User information
+        /// </summary>
         public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Table that contains Project information
+        /// </summary>
         public DbSet<Project> Projects { get; set; }
+
+        /// <summary>
+        /// Table that contains Milestone information
+        /// </summary>
         public DbSet<Milestone> Milestones { get; set; }
 
+        /// <summary>
+        /// Empty constructor that initialises the database
+        /// </summary>
+        /// <param name="options"></param>
         public MilestoneDb(DbContextOptions<MilestoneDb> options) : base(options)
         {
         }
@@ -28,6 +47,10 @@ namespace CodedMilePlanner.NewFolder
 
     }
 
+    /// <summary>
+    /// Class that will actually create the MilestoneDb using the connection string specified
+    /// in appsettings.json
+    /// </summary>
     public class DataContextFactory : IDesignTimeDbContextFactory<MilestoneDb>
     {
         public MilestoneDb CreateDbContext(string[] args)
