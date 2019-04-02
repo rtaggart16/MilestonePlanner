427c9d195cdf42cb5e6de461153391fde5ddf243
rtaggart16 <rosstaggart44@gmail.com>
2019-04-02 11:51:37 +0100

    Created the MilestoneDb class that contains initialisers for the Users, Projects and Milestones tables

---
+++ CodedMilePlanner/NewFolder/MilestoneDb.cs
@@ -0,0 +1,46 @@
+﻿using CodedMilePlanner.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodedMilePlanner.NewFolder
+{
+    public class MilestoneDb : IdentityDbContext<User>
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Milestone> Milestones { get; set; }
+
+        public MilestoneDb(DbContextOptions<MilestoneDb> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+
+    public class DataContextFactory : IDesignTimeDbContextFactory<MilestoneDb>
+    {
+        public MilestoneDb CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<MilestoneDb>();
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+              //Get correct root path
+              .SetBasePath(Directory.GetCurrentDirectory().ToString())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .Build();
+
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new MilestoneDb(builder.Options);
+        }
+    }
+}
