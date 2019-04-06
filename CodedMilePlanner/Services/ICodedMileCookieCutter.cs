b42c6c3c530afdba09127d5888b2c6d2eab85d2a
rtaggart16 <rosstaggart44@gmail.com>
2019-04-06 10:04:54 +0100

    Created Skeletal Cookie Cutter System for assigning User cookies

---
+++ CodedMilePlanner/Services/ICodedMileCookieCutter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodedMilePlanner.Services
+{
+    public interface ICodedMileCookieCutter
+    {
+    }
+
+    public class CodedMileCookieCutter : ICodedMileCookieCutter
+    {
+        public bool CreateCodedMileCookie()
+        {
+
+        }
+    }
+
+    public enum CodedMileCookieTypes
+    {
+        Authorisation = 0
+    }
+}
