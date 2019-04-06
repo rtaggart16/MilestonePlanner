a804088224342a7e929e272c276c241365fb7c69
rtaggart16 <rosstaggart44@gmail.com>
2019-04-06 10:40:17 +0100

    Added attributes to the UserAuthorisationToken class

--- CodedMilePlanner/Models/UserAuthorisationToken.cs
+++ CodedMilePlanner/Models/UserAuthorisationToken.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,14 @@ namespace CodedMilePlanner.Models
 {
     public class UserAuthorisationToken
     {
+        [Key]
+        public string ID { get; set; }
+
+        public string User_ID { get; set; }
+
+        public string Value { get; set; }
+
+        [ForeignKey("User_ID")]
+        public virtual User User { get; set; }
     }
 }
